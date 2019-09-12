3d87b82085c23d5f55421009bda10ec329c6f1a6
Matthias Osswald <m@osswald.li>
2019-09-13 01:04:39 +0200

    Merge the two setup scripts.

--- README.md
+++ README.md
@@ -1,25 +1,15 @@
-# dot_files
+# Setup Manjaro (i3wm)
 
-Dotfiles for a linux setup
+Dotfiles for a manjaro setup.
 
 ## Setup dotfiles
-
-```
-git clone https://github.com/busykoala/dot_files ~/dot_files
-cd ~/dot_files
-chmod +x setup.sh
-./setup.sh
-```
-
-## Setup manjaro
 
 Get Manjaro with i3wm from `https://manjaro.org/download/i3/`.
 Username has to be busykoala!
 
 ```
+git clone https://github.com/busykoala/dot_files ~/dot_files
 cd ~/dot_files
-chmod +x setup.sh
 chmod +x setup_manjaro.sh
-./setup.sh
 sudo ./setup_manjaro.sh
 ```
--- setup_manjaro.sh
+++ setup_manjaro.sh
@@ -24,8 +24,12 @@ pacman -S --needed --noconfirm zsh zsh-completions
 pacman -S --needed --noconfirm clipit
 # web browser
 pacman -S --needed --noconfirm firefox
+# install vim dependencies
+pacman -S --needed --noconfirm fzf the_silver_searcher astyle
+# install docker
+pacman -S --needed --noconfirm docker
 # install some programming stuff
-pacman -S --needed --noconfirm mono, go, jdk8-openjdk, cmake, clang
+pacman -S --needed --noconfirm mono, go, jdk8-openjdk, cmake, clang, r
 # install nvm from aur
 pacman -S --needed --noconfirm base-devel make cmake
 # install rust
@@ -52,6 +56,13 @@ else
 	echo "The rbenv repos exists, it is getting pulled"
 	git -C $RBENV_BUILD_REPO_FOLDER pull
 fi
+# install exa (optimized ls)
+if hash exa 2>/dev/null; then
+    echo "exa exists already"
+else
+    cargo install exa
+fi
+source /home/busykoala/.cargo/env
 
 ############
 # Settings
@@ -79,4 +90,58 @@ su - busykoala -c "rbenv global 2.6.2"
 # install scss_lint for vim
 gem install scss_lint
 
-echo "*** Stuff successfully done ***"
+####################
+# Dotfiles and Vim
+####################
+
+# combine vim repository
+VIM_REPO_FOLDER=/home/busykoala/vim_config
+VIM_REPO_URL=git@github.com:busykoala/vim_config.git
+if [ ! -d "$VIM_REPO_FOLDER" ] ; then
+	echo "The vim repo gets cloned"
+	su - busykoala -c "git clone $VIM_REPO_URL $VIM_REPO_FOLDER"
+else
+	echo "The vim repos exists, it is getting pulled"
+	su - busykoala -c "git -C $VIM_REPO_FOLDER pull"
+fi
+
+# Symlink .vimrc
+ln -sf /home/busykoala/vim_config/.vimrc /home/busykoala/.vimrc
+# Symlink does not work
+cp -f /home/busykoala/vim_config/.editorconfig /home/busykoala/.editorconfig
+# setup YCM
+YCM_REPO_FOLDER=/home/busykoala/.vim/bundle/Vundle.vim
+YCM_REPO_URL=https://github.com/gmarik/Vundle.vim.git
+if [ ! -d "$YCM_REPO_FOLDER" ] ; then
+	echo "The YCM repo gets cloned"
+	git clone $YCM_REPO_URL $YCM_REPO_FOLDER
+else
+	echo "The YCM repos exists, it is getting pulled"
+	git -C $YCM_REPO_FOLDER pull
+fi
+# YCM Configuration information
+echo ""
+echo "#######################################################"
+echo "Please see https://github.com/Valloric/YouCompleteMe"
+echo "for more information about YCM."
+echo ""
+echo "R linter will not be installed. This has to be done manually."
+echo "See https://github.com/busykoala/vim_config for more info"
+echo "######################################################"
+# ask before installing YCM
+read -p "Do you want to execute YCM installer? [Ny]" -n 1 -r
+echo    # (optional) move to a new line
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+    su - busykoala -c "cd /home/busykoala/.vim/bundle/YouCompleteMe && python3 install.py --all --clangd-completer"
+fi
+# install vim plugins
+su - busykoala -c "vim -c 'PluginInstall' -c 'qa!'"
+su - busykoala -c "vim -c 'PluginClean' -c 'qa!'"
+# Symlink dotfiles
+ln -sf /home/busykoala/dot_files/.gitconfig /home/busykoala/.gitconfig
+ln -sf /home/busykoala/dot_files/.gitignore /home/busykoala/.gitignore
+ln -sf /home/busykoala/dot_files/.zshrc /home/busykoala/.zshrc
+ln -sf /home/busykoala/dot_files/.isort.cfg /home/busykoala/.isort.cfg
+
+echo "Sucessfully finished!"
